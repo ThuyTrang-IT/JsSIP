7aa507c04cbaf9fbd424c2b655ff8c1abe84c5a1
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-06-05 10:02:41 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import './App.css';
-
+import JsSIP from 'jssip'; //thêm cái này
 import React, { useState } from 'react';
 
-function Numpad(props) {
+function Numpad() {
+  const [session, setSession] = useState(null);//thêm cái này
   const [inputValue, setInputValue] = useState("");
 
-  // hàm xử lý sự kiện khi người dùng nhấn một phím số
   const handleInput = (value) => {
     setInputValue(inputValue + value);
   }
 
-  // hàm xử lý sự kiện khi người dùng nhấn phím xóa
   const handleDelete = () => {
     setInputValue(inputValue.slice(0, -1));
   }
+
+  //thêm xử lý khi ấn nút Call
+  const handleCall = (inputValue) => {
+    const phoneNumber = inputValue; // inputValue là số điện thoại nhập vào từ bàn phím, khi ấn nút call này thì sdt gán vào biến "phoneNumber" 
+
+    const socket = new JsSIP.WebSocketInterface('wss://gc03-pbx.tel4vn.com:7444');//thay đổi này thành socket của Gcalls
+    const configuration = {
+        sockets: [socket],
+        uri: 'sip:101@2-test1.gcalls.vn:50061',
+        password: 'test1101',
+    };
+    const userAgent = new JsSIP.UA(configuration);
+
+    userAgent.start();
+
+    const callSession = userAgent.call(phoneNumber); 
+    setSession(callSession); //call tới số điện thoại:"phoneNumber"
+
+    callSession.on('connecting', () => {
+        console.log('Connecting...');
+        console.log('đang kết nối đến:'+ phoneNumber);
+    });
+
+    callSession.on('accepted', () => {
+      console.log('Connected');
+    });
+
+    callSession.on('ended', () => {
+      console.log('Kết thúc cuộc gọi');
+    });
+
+    callSession.on('failed', () => {
+      console.log('Kết thúc cuộc gọi');
+    });
+
+    callSession.connection.addEventListener('addstream', (e) => {
+        const audio = new Audio();
+        audio.srcObject = e.stream;
+        audio.play();
+    });
+  };
 
   return (
     <div className="numpad">
@@ -36,7 +76,7 @@ function Numpad(props) {
       <div className="row">
         <button onClick={() => handleDelete()}>Delete</button>
         <button onClick={() => handleInput("0")}>0</button>
-        <button onClick={() => props.onSubmit(inputValue)}>Call</button>
+       <button onClick={() => handleCall(inputValue)}>Call</button> {/* Thay đổi chỗ này! Khi ấn vào nút Call thì Onclick sẽ gọi hàm handleCall với tham số là inputValue(số điện thoại) */}
       </div>
     </div>
   );
--- src/JsSIP.js
+++ src/JsSIP.js
@@ -1,35 +1,35 @@
-import JsSIP from 'jssip';
-/////chu thich
-const socket = new JsSIP.WebSocketInterface('wss://sbc03.tel4vn.com:7444'); // thay đổi địa chỉ SIP server tương ứng
-const configuration = {
-    sockets: [socket],
-    uri: '101@2-test1.gcalls.vn:50061',
-    password: 'test1101'
-};
-const userAgent = new JsSIP.UA(configuration);
+// import { JsSIP } from 'jssip'; //sửa lại cái này
+// /////chu thich
+// const socket = new JsSIP.WebSocketInterface('wss://sbc03.tel4vn.com:7444'); // thay đổi địa chỉ SIP server tương ứng
+// const configuration = {
+//     sockets: [socket],
+//     uri: '101@2-test1.gcalls.vn:50061',
+//     password: 'test1101'
+// };
+// const userAgent = new JsSIP.UA(configuration);
 
-userAgent.start();
+// userAgent.start();
 
-const session = userAgent.invite('sip:destination@example.com', {
-    mediaConstraints: {
-      audio: true,
-      video: false,
-    },
-  });
+// const session = userAgent.invite('sip:' + inputValue + '@2-test1.gcalls.vn:50061', {
+//     mediaConstraints: {
+//       audio: true,
+//       video: false,
+//     },
+//   });
   
-  // Xử lý các sự kiện của cuộc gọi
-  session.on('connecting', (e) => {
-    console.log('Đang kết nối...');
-  });
+//   // Xử lý các sự kiện của cuộc gọi
+//   session.on('connecting', (e) => {
+//     console.log('Đang kết nối...');
+//   });
   
-  session.on('progress', (e) => {
-    console.log('Đang kết nối...');
-  });
+//   session.on('progress', (e) => {
+//     console.log('Đang kết nối...');
+//   });
   
-  session.on('accepted', (e) => {
-    console.log('Cuộc gọi đã được chấp nhận');
-  });
+//   session.on('accepted', (e) => {
+//     console.log('Cuộc gọi đã được chấp nhận');
+//   });
   
-  session.on('ended', (e) => {
-    console.log('Cuộc gọi đã kết thúc');
-  });
+//   session.on('ended', (e) => {
+//     console.log('Cuộc gọi đã kết thúc');
+//   });
